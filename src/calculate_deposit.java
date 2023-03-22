84ba3bc43b202f0c8881f8264794d29ead91252a
Алексей Кузнецов <teaguest@yandex.ru>
2023-03-22 19:10:34 +0300

    f

--- src/calculate_deposit.java
+++ src/calculate_deposit.java
@@ -1,22 +1,21 @@
 import java.net.URI;import java.util.Scanner;import java.io.IOException;
 
-public class calculate_deposit
-{
-double Calculate_Complex_Percent_Function(double a, double y,int d ) {
-       double pay = a * Math.pow((1 + y/ 12), 12 *d);
-          return rnd(pay, 2);
-  } double Calculate_Simple_Percent_Function(double doubleAmount,double double_year_rate, int deposit_period) {
-         return rnd(doubleAmount+doubleAmount * double_year_rate *deposit_period, 2);
-    } double rnd(double value
-  ,int places) {
-       double ScaLe= Math.pow
-          (10, places);
-       return Math.round(value*ScaLe)
-            /ScaLe; }
+public class calculate_deposit{
+    double Calculate_Complex_Percent_Function(double a, double y,int d ) {
+       double pay = a * Math.pow((1 + y / 12), 12 * d);
+       return rnd(pay, 2);
+    }
+    double Calculate_Simple_Percent_Function(double doubleAmount,double double_year_rate, int deposit_period) {
+       return rnd(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
+    }
+    double rnd(double value, int places) {
+       double ScaLe= Math.pow(10, places);
+       return Math.round(value * ScaLe) / ScaLe;
+    }
 
-    void do_important_job( )
-    {
-      int period, action ;
+    void do_important_job( ){
+      int period,
+              action;
         Scanner abcdef = new Scanner(System.in); System.out.println("Введите сумму вклада в рублях:") ;
       int amount = abcdef.nextInt(); System.out.println("Введите срок вклада в годах:") ;
         period = abcdef.nextInt( );
